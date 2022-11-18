87d921ae288b62ef0988ab74ac9d59471dedfb08
Julien Jollant <julien.jollant.sio@gmail.com>
2022-11-18 15:15:45 +0100

    add note attribute for shop pickup

---
+++ src/Model/Order/NoteAttribute.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Numiscorner\MarketplaceBundle\Model\Order;
+
+class NoteAttribute
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
